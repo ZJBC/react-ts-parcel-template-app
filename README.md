5c185cbc655294478524f2eea315596d73d2f31c
yurt <sherferyu@163.com>
2019-01-18 15:02:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-parcel-template-app
+
+    react-ts-parcel-cli的应用开发模版
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  (
+    <div>
+      React + TypeScript
+    </div>
+  ),
+  document.getElementById('root')
+)
